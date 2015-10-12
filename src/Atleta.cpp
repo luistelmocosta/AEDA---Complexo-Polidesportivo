7cad49bb642fd1dde65061abd3c4eceb638dc13e
luistelmocosta <luistelmocosta@gmail.com>
2015-10-12 23:33:11 +0100

    Atletas.cpp | Read/Write File

---
+++ src/Atleta.cpp
@@ -0,0 +1,83 @@
+#include "Atleta.h"
+
+using namespace std;
+
+unsigned int Atleta::ultimoID = 0;
+
+Atleta::Atleta(string Nome, string Pais, info Peso, info Altura, int Pontuacao) :
+		nome(Nome), pais(Pais), peso(Peso), altura(Altura), pontuacao(Pontuacao), id(++ultimoID) {}
+
+int Atleta::imprime() const{
+
+	int n = 5;
+
+	cout << "Nome: " << nome << endl;
+	cout << "Pais: " << pais << endl;
+	cout << "Peso: " << peso << endl;
+	cout << "Altura: " << altura << endl;
+	cout << "Pontuacao: "<< pontuacao << endl;
+
+	return n;
+}
+
+unsigned int Atleta::getID() const{
+	return id;
+}
+
+string Atleta::getNome() const{
+	return nome;
+}
+
+string Atleta::setNome(string nnome)const{
+	nome = nnome;
+}
+
+void Atleta::setPontuacao(int ptc){
+	pontuacao = ptc;
+
+	if(ptc < 0)
+		throw ValorIncorrecto(ptc);
+}
+
+float getPontuacao() const{
+	return pontuacao;
+}
+
+vector<Prova*> Atleta::getProvas() const{
+	return provas;
+}
+
+vector<Modalidade*>Atleta::getModalidades()const{
+	return modalidades;
+}
+
+bool Atleta::eliminaProva(string prova){
+	for (unsigned int i = 0; i < provas.size(); i++){
+		if(provas[i] == prova){
+			provas.erase(provas.begin() + i);
+			return true;
+		}
+	}
+	return false;
+}
+
+void Atleta::readFile(ifstream& ficheiro_leitura){
+	if(!ficheiro_leitura)
+		throw ErroNoFicheiro();
+	else{
+		string nome, pais, temp1, temp2;
+		string pais;
+		info peso;
+		info altura;
+
+		getline(ficheiro_leitura, nome);
+		getline(ficheiro_leitura, pais);
+		getline(ficheiro_leitura, temp1);
+		getline(ficheiro_leitura, temp2);
+
+		peso = atoi(temp1.c_str());
+
+
+
+	}
+}
--- src/Atleta.h
+++ src/Atleta.h
@@ -15,22 +15,40 @@ class Atleta {
 	string pais;
 	info peso;
 	info altura;
+	float pontuacao;
+	static unsigned int ultimoId;
+	unsigned int id;
 
 
 public:
 	Atleta(string nome, string pais, info peso, info altura);
-	int getID() const;
-	int setID() const;
 	void imprime() const;
 	string getNome() const;
-	string setNome() const;
+	string setNome(string nnome) const;
 	int getPontuacao();
-	int setPontuacao();
-	int getAltura() const;
-	int getPeso() const;
+	void setPontuacao();
+	unsigned int getAltura() const;
+	unsigned int getPeso() const;
 	vector<Modalidade *> modalidades;
 	vector<Prova*> provas;
 
+	class ValorIncorrecto {
+	public:
+		int v;
+		ValorIncorrecto(int v) :
+			v(v) {
+		}
+	};
+
+
+	void readFile(ifstream& ficheiro_leitura);
+	void writeFile(ofstream& ficheiro_escrita);
+
+	class ErroNoFicheiro{
+	public:
+		ErroNoFicheiro(){}
+
+	};
 };
 
 
