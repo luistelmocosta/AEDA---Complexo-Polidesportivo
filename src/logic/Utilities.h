dfc2473066f6317ec76103d72afdab2b651f9a25
01alex <xanomcr@gmail.com>
2015-11-02 17:57:23 +0000

    Utilities.h updated

--- src/logic/Utilities.h
+++ src/logic/Utilities.h
@@ -1,7 +1,17 @@
 #ifndef SRC_UTILITIES_H_
 #define SRC_UTILITIES_H_
 
+#include <vector>
+
 using namespace std;
+
+
+
+/*
+ 	 *						*
+ 	 * 		UTILITARIOS		*
+ 	 *						*
+*/
 
 struct info{				// CC atleta
 	string nome;
@@ -12,7 +22,6 @@ struct info{				// CC atleta
 };
 
 
-
 struct date{				// a usar em prova, campeonato
 	unsigned int dia;
 	unsigned int mes;
@@ -20,6 +29,48 @@ struct date{				// a usar em prova, campeonato
 };
 
 
+
+
+
+/*
+ 	 *			ALGORITMOS			*
+ 	 * 		PESQUISA e ORDENACAO	*
+ 	 *			VECTORES			*
+*/
+
+template <class Comparable>
+int sequentialSearch(const vector<Comparable> &v, Comparable x)
+{
+    for (unsigned int i = 0; i < v.size(); i++)
+        if(v[i] == x)
+           return i;   // encontrou
+    return -1;     // nao encontrou
+}
+
+
+
+template <class Comparable>
+void insertionSort(vector<Comparable> &v)
+{
+    for (unsigned int p = 1; p < v.size(); p++)
+    {
+    	Comparable tmp = v[p];
+    	int j;
+    	for (j = p; j > 0 && tmp < v[j-1]; j--)
+    		v[j] = v[j-1];
+    	v[j] = tmp;
+    }
+}
+
+
+
+
+
+/*
+	 * 		TRATAMENTO			*
+	 * 			de				*
+	 * 		EXCEPCOES			*
+*/
 
 class ValorIncorrecto {
 	public:
