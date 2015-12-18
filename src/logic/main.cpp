dc64257922177f7802d46f8414329044b55ab8eb
01alex <xanomcr@gmail.com>
2015-12-18 15:38:08 +0000

    fix

---
+++ src/logic/main.cpp
@@ -0,0 +1,38 @@
+#include "Menu.h"
+
+int main(){
+
+	Menu menu;
+	menu.initMenu();
+
+	return 0;
+}
+
+
+/*
+ *
+ * 	NAO APAGAR
+ *
+	Campeonato c;
+
+	vector<unsigned> eq1, eq2, eq3;
+	eq1.push_back(10);
+	eq2.push_back(100);
+	eq3.push_back(50);
+
+	Equipa e1, e2, e3;
+	e1.setMedalhas(eq1);
+	e2.setMedalhas(eq2);
+	e3.setMedalhas(eq3);
+
+	c.inserirEquipa(e1);
+	c.inserirEquipa(e2);
+	c.inserirEquipa(e3);
+
+	c.ordenaClassificacoes();
+
+	cout << c.getEquipas()[0]->getMedalhas()[0] << c.getEquipas()[1]->getMedalhas()[0] << c.getEquipas()[2]->getMedalhas()[0];
+*/
+
+
+
